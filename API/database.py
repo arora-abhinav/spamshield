9afa4082a3e5b218be15c8e830b1f621b32b5748
Abhinav Arora <aroraabhinav2007@gmail.com>
2026-05-27 12:47:49 +0530

    Test

--- API/database.py
+++ API/database.py
@@ -3,3 +3,9 @@ import os
 
 DATABASE_URL = os.getenv("CONNECTION_STRING") or os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
+
+import os
+print(f"CONNECTION_STRING: {os.getenv('CONNECTION_STRING')}")
+print(f"DATABASE_URL: {os.getenv('DATABASE_URL')}")
+connection_string = os.getenv("CONNECTION_STRING") or os.getenv("DATABASE_URL")
+print(f"Using: {connection_string}")
