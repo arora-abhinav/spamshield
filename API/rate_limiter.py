03bdd7cc839ce90c7cf7a0ced444a7b8b272245a
Abhinav Arora <aroraabhinav2007@gmail.com>
2026-06-06 23:45:53 +0530

    Fixed some more bugs regarding rate limiting

--- API/rate_limiter.py
+++ API/rate_limiter.py
@@ -1,26 +1,24 @@
-from typing import Annotated
-
-from fastapi import Depends, Request
-from fastapi.security import HTTPBearer
+from fastapi import Request
 from slowapi.util import get_remote_address
 from slowapi import Limiter
 from jose import jwt
 
 import auth
 
-security_optional = HTTPBearer(auto_error=False)
-
 #Simply returns the device_id from the header. If device_id isn't present
 #Then falls back to the ip_address
-def get_device_header(header: Annotated[auth.HTTPAuthorizationCredentials, Depends(security_optional)], request: Request):
-    try:
-        payload = jwt.decode(header.credentials, auth.SECRET_KEY, auth.ALGORITHM)
-        device_id = payload.get("device_id")
-        if device_id is None:
-            return get_remote_address(request)
-    except:
-        return get_remote_address(request)
-    return device_id
+def get_device_header(request: Request):
+    auth_header = request.headers.get("Authorization")
+    if auth_header and auth_header.startswith("Bearer "):
+        token = auth_header[7:]
+        try:
+            payload = jwt.decode(token, auth.SECRET_KEY, [auth.ALGORITHM])
+            device_id = payload.get("device_id")
+            if device_id:
+                return device_id
+        except Exception:
+            pass
+    return get_remote_address(request)
 
 
 #limiter wil be imported into both auth.py and main.py
